776b01b7f874c3bf34d18b3c2be2e0e197c71653
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2026-02-19 15:20:27 +0900

    autogen.sh to retrieve source files

---
+++ autogen.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+git submodule update --init Libc-825.24
+git submodule update --init --reference Libc-825.24 Libc-498.1.7
+git submodule update --init --reference Libc-825.24 Libc-498
+
+git submodule update --init Libm-292.4
+
+git submodule update --init xnu-1456.1.26
+git submodule update --init --reference xnu-1456.1.26 xnu-1228.15.4
+git submodule update --init --reference xnu-1456.1.26 xnu-1228
